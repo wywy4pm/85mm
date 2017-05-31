c4b5123d0be671663d9f75c326e17332e4845031
wywy4pm <wywywy996@gmail.com>
2017-05-31 18:11:25 +0800

    push

---
+++ 85mm/app/src/main/java/com/arun/a85mm/helper/PushHelper.java
@@ -0,0 +1,34 @@
+package com.arun.a85mm.helper;
+
+import android.app.Notification;
+import android.content.Context;
+
+import com.umeng.message.PushAgent;
+import com.umeng.message.UmengAdHandler;
+import com.umeng.message.UmengMessageHandler;
+import com.umeng.message.UmengNotificationClickHandler;
+import com.umeng.message.entity.UMessage;
+
+/**
+ * Created by wy on 2017/5/31.
+ */
+
+public class PushHelper {
+    public static void setPushNotification(PushAgent mPushAgent) {
+        UmengNotificationClickHandler notificationClickHandler = new UmengNotificationClickHandler() {
+            @Override
+            public void dealWithCustomAction(Context context, UMessage msg) {
+
+            }
+        };
+        mPushAgent.setNotificationClickHandler(notificationClickHandler);
+
+        /*UmengMessageHandler umengAdHandler = new UmengMessageHandler() {
+            @Override
+            public Notification getNotification(Context context, UMessage uMessage) {
+                return super.getNotification(context, uMessage);
+            }
+        };
+        mPushAgent.setMessageHandler(umengAdHandler);*/
+    }
+}
